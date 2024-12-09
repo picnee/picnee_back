5fdfbfaf03902bd0b5da1a9e0d2e02229005b993
leebuwon <bw1611@naver.com>
2024-12-09 17:47:15 +0900

    test : 댓글 수정 테스트

--- src/test/java/com/picnee/travel/domain/postComment/service/PostCommentServiceTest.java
+++ src/test/java/com/picnee/travel/domain/postComment/service/PostCommentServiceTest.java
@@ -7,7 +7,9 @@ import com.picnee.travel.domain.post.entity.CreateTestPost;
 import com.picnee.travel.domain.post.entity.Post;
 import com.picnee.travel.domain.post.service.PostService;
 import com.picnee.travel.domain.postComment.dto.req.CreatePostCommentReq;
+import com.picnee.travel.domain.postComment.dto.req.UpdatePostCommentReq;
 import com.picnee.travel.domain.postComment.entity.PostComment;
+import com.picnee.travel.domain.postComment.exception.NotValidOwnerException;
 import com.picnee.travel.domain.user.dto.req.AuthenticatedUserReq;
 import com.picnee.travel.domain.user.entity.CreateTestUser;
 import com.picnee.travel.global.jwt.dto.res.JwtTokenRes;
@@ -21,6 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.picnee.travel.global.exception.ErrorCode.NOT_VALID_OWNER_EXCEPTION;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -63,6 +66,31 @@ class PostCommentServiceTest {
         assertThat(postComment).isNotNull();
         assertThat(postComment.getContent()).isEqualTo("댓글입니다.");
         assertThat(postComment.getUser().getId()).isEqualTo(user.getId());
+    }
+
+    @Test
+    @DisplayName("댓글을 수정한다.")
+    void test2() {
+        UpdatePostCommentReq req = UpdatePostCommentReq.builder()
+                .content("수정된 댓글입니다.")
+                .build();
+
+        postCommentService.update(post.getId(), postComment.getId(), req, user);
+
+        assertThat(postComment).isNotNull();
+        assertThat(postComment.getContent()).isEqualTo("수정된 댓글입니다.");
+        assertThat(postComment.getUser().getId()).isEqualTo(user.getId());
+    }
+
+    @Test
+    @DisplayName("댓글 수정 테스트 실패 : 다른 사람의 댓글은 수정하지 못한다.")
+    void test3() {
+        UpdatePostCommentReq req = UpdatePostCommentReq.builder()
+                .content("수정된 댓글입니다.")
+                .build();
+
+        assertThatThrownBy(() -> postCommentService.update(post.getId(), postComment.getId(), req, anotherUser))
+                .isInstanceOf(NotValidOwnerException.class);
     }
 
 }
